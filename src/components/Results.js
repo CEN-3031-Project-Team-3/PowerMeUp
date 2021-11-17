5ffb3f91f95593e9c00b7b0995bec446bae197d3
Timothy Schneider <timothycschneider@outlook.com>
2021-11-17 16:54:55 -0500

    added another singleton to store results of calculation, set up results to display this

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,40 @@
 import React from "react";
 import {Row, Col, Button, Stack} from "react-bootstrap/";
 import { NavLink } from 'react-router-dom';
+import results_record from "./ResultsRecord";
 
 export default function Results(){
+    const index = results_record.getData();
+    console.log(index)
+    const maxIndex = 1000;
+
+    const ratio = index/maxIndex;
 
     return (
-        
+        <Stack>
+        <Row className="justify-content-md-center">
+            <Col sm="auto">
+              <h1>Results Page </h1>
+            </Col>
+          </Row>
+
+        <Row className="justify-content-md-center">
+            <Col sm="auto">
+                <div className="App">
+                    <header className="App-results medium">
+                            <h1>Your Power Index</h1>
+                            <p></p>
+                            <p>is: </p>
+                            <h1> {ratio}</h1>
+                            
+                            <p> on a scale of 1 - 1000</p> 
+                    </header>
+                </div>
+            </Col>
+        </Row>
+        </Stack>
+
+        /*
         <Stack gap={4}>
 
           <Row className="justify-content-md-center">
@@ -75,6 +104,6 @@ export default function Results(){
         </Row>
             
         </Stack>
-        
+        */
       );
 }
